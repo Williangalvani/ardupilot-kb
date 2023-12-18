840f722644e783c76f15a986a35759afa6e31e8d
Williangalvani <Williangalvani@users.noreply.github.com>
2023-12-18 22:03:08 +0000

    deploy: ef1e447e37be2211a14738440f30c6d48af88c9c

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 1;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '108894773717780639025439472017689825294'                       // used to check whether the localStorage data is stale
+var gzip_hash = '236043128922562168371559054076439593575'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
